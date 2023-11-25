b4a727859fbe484cefae9934f04aa7bad910f91d
BusaidiEssa <116034947+BusaidiEssa@users.noreply.github.com>
2023-11-25 17:12:08 +0400

    Add files via upload

---
+++ O'reilly 50 projects/Waving Form/script.js
@@ -0,0 +1,9 @@
+const label= document.querySelectorAll('.form-control label')
+
+
+labels.forEach(label => {
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter,idx) => `<span>${letter}/span>` )
+    .join('')
+});
