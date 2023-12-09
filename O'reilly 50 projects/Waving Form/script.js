6b00ce5314a398e4ea3478e967cc38985a4ab11e
BusaidiEssa <116034947+BusaidiEssa@users.noreply.github.com>
2023-12-09 12:37:18 +0400

    Delete O'reilly 50 projects/Waving Form directory

--- O'reilly 50 projects/Waving Form/script.js
+++
@@ -1,9 +0,0 @@
-const label= document.querySelectorAll('.form-control label')
-
-
-labels.forEach(label => {
-    label.innerHTML = label.innerText
-    .split('')
-    .map((letter,idx) => `<span>${letter}/span>` )
-    .join('')
-});
